a4412e959164f4fdbf9f6084495256b33417fdf2
Abhi05kk <abhiramnambiar25@gmail.com>
2025-01-13 14:49:41 +0530

    Update Users.jsx

--- admin/src/pages/Users.jsx
+++ admin/src/pages/Users.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 
 const Users = ({ }) => {
 
-  const url = "http://localhost:4000"
+  const url = "https://threads-backend-9ij1.onrender.com
   const [Users, setUsers] = useState([])
 
   const fetchUsers = async () => {
